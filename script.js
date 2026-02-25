e13fd2d450027c77b982f8d49a0d811c601a2154
Joey Cucco <87213402+CuccoForCoding@users.noreply.github.com>
2026-02-25 14:34:25 -0500

    variables set for cart
    photos edited. gifs created. jc

--- script.js
+++ script.js
@@ -17,12 +17,36 @@ for (let item of navColor) {
 
 //Clean light/dark mode toggle with a .darkMode and .blend CSS class 
 //uses global style variable var(--dark-gray) for background when clicked and also changes text color to var(--green)
-//using const instead of let because i'm not reassigning toggleButton 
+//using const instead of let because I'm not reassigning toggleButton 
 const toggleButton = document.getElementById("lightDarkModeBTN");
 
 toggleButton.addEventListener("click", function() {
     document.body.classList.toggle("darkMode");
 });
+
+//THIS IS THE CODE/LOGIC FOR THE SHOPPING CART
+//I AM ONLY DISPLAYING TO THE CONSOLE AND TO THE PAGE
+//NO localStorage VARIABLES USED OR PAYMENT PROCESSING
+
+let cart = [];
+
+const taxRate = 0.06;
+const flatShippingRate = 25;
+
+const addToCartButtons = document.querySelector(".addToCartBTN");
+const cartItemList = document.querySelector(".cartItemList");
+const subTotalValue = document.getElementById("subTotalValue");
+const taxValue = document.getElementById("taxValue");
+const shippingValue = document.getElementById("shippingValue");
+const totalValue = document.getElementById("totalValue");
+const checkoutBtn = document.getElementById("checkoutBtn");
+
+for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", function() {
+        const productItem = addToCartButtons[i];
+        
+    });
+}
 
 
 
